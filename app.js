367e6ec8c8d97b97a387f6088695291b23fc94ba
Enes Dayanç <enesdayanc@gmail.com>
2016-02-20 10:56:48 +0200

    connect mongo db

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require("mongoose");
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -13,6 +14,9 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+//connect mongo Db
+mongoose.connect('mongodb://testUser:test@ds031641.mongolab.com:31641/heroku_bl17m7xv');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
