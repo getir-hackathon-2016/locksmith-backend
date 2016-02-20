569ab98dfdf42d29bd1d5acd06c4edd7d8826915
Enes Dayanç <enesdayanc@gmail.com>
2016-02-20 19:43:05 +0200

    edit mongo db connetion string

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     'secret': '0xm0w4rBPVg4iLDGni2ehD2l7u2yUV8b',
-    'database': 'mongodb://testUser:test@ds031641.mongolab.com:31641/heroku_bl17m7xv'
+    'database': 'mongodb://testUser:test@ds013738.mongolab.com:13738/heroku_r0kw59sb'
 };
